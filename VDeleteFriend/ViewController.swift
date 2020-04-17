49202cb47fcd0e52b91ce49743240cf8975d5f51
Ivan Novikov <vanchell370@gmail.com>
2020-04-17 22:26:04 +0300

    Initial Commit

---
+++ VDeleteFriend/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VDeleteFriend
+//
+//  Created by Иван Новиков on 17.04.2020.
+//  Copyright © 2020 Иван Новиков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
